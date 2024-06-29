eb2415bbac0345e22f84897d977ab97575a9dee1
Kenneth Tay <kjytay@gmail.com>
2024-06-29 00:11:11 -0700

    Set up basic structure of package.

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='py-puzzles',
+    url='https://github.com/kjytay/py-puzzles',
+    author='Jingyi Kenneth Tay',
+    author_email='kjytay@gmail.com',
+    description='My collection for generating and solving puzzles',
+    packages=['ktaypuzzles'],
+    version='0.1.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
