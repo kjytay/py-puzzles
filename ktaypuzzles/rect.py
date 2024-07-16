662cf744512a9571381cf5dbfc1942c71124c814
Kenneth Tay <kjytay@gmail.com>
2024-07-16 13:39:12 +0800

    Make Rect a dataclass.

--- ktaypuzzles/rect.py
+++ ktaypuzzles/rect.py
@@ -1,18 +1,21 @@
-class Rect:
+from dataclasses import dataclass
 
-    def __init__(self, r1: int, r2: int, c1: int, c2: int):
-        """
-        Rectangle is defined by first and last row index of the rectangle (r1, r2)
-        and first and last column index of the rectangle (c1, c2).
-        In this implementation, think of each (r,c) index as representing a
-        box rather than a point.
-        """
-        assert r1 <= r2, 'r1 ({}) must be <= r2 ({})'.format(r1, r2)
-        assert c1 <= c2, 'c1 ({}) must be <= c2 ({})'.format(c1, c2)
-        self.r1 = r1
-        self.r2 = r2
-        self.c1 = c1
-        self.c2 = c2
+@dataclass
+class Rect:
+    """
+    Rectangle is defined by first and last row index of the rectangle (r1, r2)
+    and first and last column index of the rectangle (c1, c2).
+    In this implementation, think of each (r,c) index as representing a
+    box rather than a point.
+    """
+    r1: int
+    r2: int
+    c1: int
+    c2: int
+
+    def __post_init__(self):
+        assert self.r1 <= self.r2, 'r1 ({}) must be <= r2 ({})'.format(self.r1, self.r2)
+        assert self.c1 <= self.c2, 'c1 ({}) must be <= c2 ({})'.format(self.c1, self.c2)
 
     def does_rect_overlap(self, other: 'Rect'):
         # check if one rectangle is above the other
@@ -24,13 +27,3 @@ class Rect:
             return False
         
         return True
-    
-    def __str__(self):
-        return 'Rect({}, {}, {}, {})'.format(self.r1, self.r2, self.c1, self.c2)
-    
-    def __eq__(self, other):
-        if not isinstance(other, Rect):
-            return NotImplemented
-
-        return (self.r1 == other.r1 and self.r2 == other.r2 and
-                self.c1 == other.c1 and self.c2 == other.c2)
--- test/test_rect.py
+++ test/test_rect.py
@@ -1,4 +1,14 @@
+import pytest
 from ktaypuzzles.rect import Rect
+
+def test_invalid_rect():
+    with pytest.raises(AssertionError) as exc_info:
+        Rect(1, 0, 1, 1)
+    assert str(exc_info.value) == 'r1 (1) must be <= r2 (0)'
+
+    with pytest.raises(AssertionError) as exc_info:
+        Rect(0, 0, 2, 1)
+    assert str(exc_info.value) == 'c1 (2) must be <= c2 (1)'
 
 def test_does_rect_overlap():
     A = Rect(0, 2, 0, 2)
